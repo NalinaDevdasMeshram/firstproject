e35d0b756dd2ef0dd2e507ac90f91e498ddd1ce3
nalina <naleenameshram85@gmail.com>
2024-06-27 12:14:34 +0530

    done

---
+++ practicesprogram/src/App.jsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+//  import ComponentOne from './ComponentOne'
+import Component from "./Components/Session/Component"
+
+function App() {
+ return (
+   <div>
+    {/* <ComponentOne/>  */}
+    <Component/> 
+   </div>
+  )
+}
+
+export default App
---
+++ practicesprogram/src/ComponentOne.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class ComponentOne extends React.Component{
+render(){
+ return(
+    <div>
+    <h1>welcome to React world</h1>
+    </div>
+   );
+}
+}
+export default ComponentOne;
